fac4263549168815197dda1f0412fc0ce8118fb9
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-09-24 19:03:41 +0700

    in ra màn hình

---
+++ cmake-build-debug/CMakeFiles/exercise.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise.dir/main.c.o"
+  "exercise.pdb"
+  "exercise.exe"
+  "libexercise.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    //in chữ
+    printf("Thao tác với C\n");
+    return 0;
+}
